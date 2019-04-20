a45d9ec08e6f43bfe212be4c4db88807b516f8e6
Mateja Petrovic <mateja176@gmail.com>
2019-04-21 01:05:12 +0200

    Adds displayName to HOC

--- src/withColor.tsx
+++ src/withColor.tsx
@@ -6,32 +6,17 @@ export interface WithColorProps {
   color: Color;
 }
 
-// export type WithColor = (
-//   color: Color,
-// ) => <Props extends WithColorProps>(
-//   Component: ComponentType<Props>,
-// ) => ComponentType<Omit<Props, 'color'>>;
-
-// export type WithColor = (
-//   color: Color,
-// ) => <Props extends WithColorProps>(
-//   Component: ComponentType<Props>,
-// ) => ComponentType<Diff<Props, WithColor>>;
-
-// const withColor: WithColor = color => Component => props => (
-//   <Component {...props} color={color} />
-// );
-
-// const withColor = (color: Color) => <Props extends WithColorProps>(
-//   Component: ComponentType<Props>,
-// ) => (props: Omit<Props, 'color'>) => (
-//   <Component {...props as Props} color={color} />
-// );
-
 const withColor = (color: Color) => <Props extends WithColorProps>(
   Component: ComponentType<Props>,
-) => (props: Diff<Props, WithColorProps>) => (
-  <Component {...props as Props} color={color} />
-);
+) => {
+  const WithColor = (props: Diff<Props, WithColorProps>) => (
+    <Component {...props as Props} color={color} />
+  );
+
+  WithColor.displayName = `${withColor.name}(${Component.displayName ||
+    Component.name})`;
+
+  return WithColor;
+};
 
 export default withColor;
